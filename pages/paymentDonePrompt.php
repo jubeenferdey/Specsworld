8d44f4fed59d19a938f56f931c05adc5e29ceecd
jubeenferdey <ferdeyjubeen@gmail.com>
2022-05-23 00:43:16 +0530

    Added the Payments Functionality :)

---
+++ pages/paymentDonePrompt.php
@@ -0,0 +1,18 @@
+<?php
+include('../libs/loading.php');
+loadTemplate("head");
+?>
+<main>
+<body>
+<section class="heading">
+    <h1>Payment Done Successfully!</h1>
+    <p> <a href="../index.php">Order</a> >> Payment</p>
+</section>
+<br><br><br><br><br>
+<center><h1>Please Return to the home page to Browse More.</h1>
+<br><br>
+<button type="button" class="btn"><a href='../home.php'>Back to Home Page</a></button>
+</center>
+</main>
+</body>
+</html>
--- pages/viewProducts.php
+++ pages/viewProducts.php
@@ -16,18 +16,26 @@ loadTemplate("adminHeader");
 <!-- Throw the below php code here -->
 <section class='banner'>    
 <?php
-        foreach ($result as $key => ['Name' => $name,'Type'=>$type, 'Power'=>$power,'Image'=>$image ]){
+        foreach ($result as $key => ['Name' => $name,'Type'=>$type, 'Power'=>$power,'Image'=>$image, 'Price' => $price ]){
           print("
+          <form action = 'paymentPage.php' method = 'POST' >
           <div class='box'>
                 <img src='$image'>
                 <div class='content'>
+                <input type = 'hidden' name = 'price' value=$price />
+                <input type = 'hidden' name = 'name' value=$name />
+                <input type = 'hidden' name = 'Type' value=$type />
+                <input type = 'hidden' name = 'power' value=$power />
                     <span>$type</span>
                     <h3>$name</h3>
                     <span>Power: $power</span>
                     <br><br>
-                    <a href='' class='btn'>Explore</a>
+                    <span>Price: $price</span>
+                    <br><br>
+                    <button class='btn' type='submit' > Buy Now </button>
                 </div>
             </div>
+            </form>
       ");
       
       }
